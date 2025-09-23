f1eefb9ab11f382196ab461fb4df3bef2b242799
EdenPhiila <luxening@gmail.com>
2025-09-23 01:13:26 -0400

    Input Asset Attempt
    I tried to get the Asset System to work with the Weapon Handler I'm making. It won't detect my scrolls for some reason and I cant figure out why. Will try more later.

--- Assets/Scripts/Weapons/WeaponHandler.cs
+++ Assets/Scripts/Weapons/WeaponHandler.cs
@@ -1,6 +1,10 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class WeaponHandler : MonoBehaviour
 {
     public GameObject starterWeapon;
@@ -8,30 +12,68 @@ public class WeaponHandler : MonoBehaviour
     //Exactly what weapons we have
     public List<GameObject> weapons = new List<GameObject>(); //List instead of an array for constant updating
     public GameObject currentWeapon; //The weapon currently in the player's hands
+    public int weaponSlot; //Which weapon in slot we're holding
     public GameObject newWeapon; //For if a player picks up a new gun.
     public Transform weaponLocation; //Where the weapon is in the player's POV
 
+    private float scrollValue;
+
     void Start()
     {
+
         weapons.Add(starterWeapon);
         currentWeapon = Instantiate(weapons[0]);
+        weaponSlot = 0;
+        
     }
 
-    void Update() 
+    void Update()
     {
         if (newWeapon != null)
         { AddWeapon(newWeapon); }
         currentWeapon.transform.position = weaponLocation.position;
-
-        Scroll();
     }
 
-    void Scroll() //When the player want's to rotate to a different weapon in their wheel
-    { 
+    //NOT WORKING! IDK WHY
+    public void OnSwitchGun(InputAction.CallbackContext ctx) //When the player want's to rotate to a different weapon in their wheel
+    {
+        scrollValue = ctx.ReadValue<float>();
+        Debug.Log(scrollValue);
+
         //If player scrolls
-        //Deactivate (not destroy) current weapon
-        //Set current weapon to next weapon
-        //Play equip animation and activate new current weapon
+        if (scrollValue > 0) //Scroll up
+        {
+            currentWeapon.SetActive(false); //Deactivate (not destroy) current weapon
+            if (weaponSlot > weapons.Count - 1) //Actually check if it's a gun. If not, change to base
+            {
+                currentWeapon = weapons[0];
+                weaponSlot = 0;
+            }
+            else
+            {
+                currentWeapon = weapons[weaponSlot + 1]; //Set current weapon to next weapon
+            }
+            currentWeapon.SetActive(true); //And activate it
+            //Play equip animation and activate new current weapon
+        }
+        else if (scrollValue < 0) //Scroll down
+        {
+            currentWeapon.SetActive(false); //Deactivate (not destroy) current weapon
+            if (weaponSlot < 0) //Actually check if it's a gun. If not, change to base
+            {
+                currentWeapon = weapons[weapons.Count];
+                weaponSlot = weapons.Count - 1;
+            }
+            else
+            {
+                currentWeapon = weapons[weaponSlot - 1]; //Set current weapon to next weapon
+            }
+            currentWeapon.SetActive(true); //And activate it
+            //Play equip animation and activate new current weapon
+        }
+
+
+
     }
 
     void AddWeapon(GameObject addWeapon)
@@ -43,6 +85,8 @@ public class WeaponHandler : MonoBehaviour
             currentWeapon = Instantiate(addWeapon); //Create the actual weapon and make it active
             currentWeapon.SetActive(true);
             newWeapon = null; //So that it doesn't endlessly add the same weapon.
+            weaponSlot = weapons.Count - 1;
+            Debug.Log(weaponSlot);
 
             //For debugging to make sure the list is updated
             foreach (GameObject item in weapons)
