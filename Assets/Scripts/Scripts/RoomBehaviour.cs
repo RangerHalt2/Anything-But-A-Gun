2c1ef2967245277305785e4005a388e03f52569c
Shawn <185832431+Shtealc@users.noreply.github.com>
2025-10-11 15:58:11 -0400

    random level gen

--- Assets/Scripts/Scripts/RoomBehaviour.cs
+++ Assets/Scripts/Scripts/RoomBehaviour.cs
@@ -6,20 +6,16 @@ public class RoomBehaviour : MonoBehaviour
 {
     public GameObject[] walls;
     public GameObject[] doors;
-    public bool[] testStatus;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        UpdateRoom(testStatus);
-    }
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
-    void UpdateRoom(bool[] status)
+    public void UpdateRoom(bool[] status)
     {
         for (int i = 0; i < status.Length; i++)
         {
