2a4f28878c7868b9c65e184e1c44abfc99d7c4d5
Shawn <185832431+Shtealc@users.noreply.github.com>
2025-10-13 16:58:05 -0400

    started work modular asset placeement

---
+++ Assets/Scripts/Scripts/RandomAssets.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RandomAssets : MonoBehaviour
+{
+    public GameObject prefab;
+    public Vector2 area;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
