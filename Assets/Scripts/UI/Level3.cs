e1292531074e400d9204d940e6c0e00741dce444
Shawn <185832431+Shtealc@users.noreply.github.com>
2025-12-01 13:08:18 -0500

    added lvl 2 and lvl 3 buttons

---
+++ Assets/Scripts/UI/Level3.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+using TMPro;
+//Script created By Anthony Mota
+public class level3 : MonoBehaviour 
+{
+
+    public void PlayGame ()
+    {
+        PlayerController controller = GameObject.FindAnyObjectByType<PlayerController>();
+        if(controller != null)
+        {
+            Destroy(controller.gameObject);
+        }
+
+        SceneManager.LoadScene("Level Gen 4");
+    }
+  
+}
