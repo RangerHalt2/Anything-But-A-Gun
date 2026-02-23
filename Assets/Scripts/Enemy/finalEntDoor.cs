72eea43d27f62c6485cf0615c642b62e9a3fd055
Shawn <185832431+Shtealc@users.noreply.github.com>
2026-02-23 17:39:25 -0500

    BOSS LVL

---
+++ Assets/Scripts/Enemy/finalEntDoor.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FinalEntDoor : MonoBehaviour
+{
+    public GameObject entranceDoor;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (entranceDoor != null)
+            {
+                entranceDoor.SetActive(true);
+            }
+        }
+    }
+}
