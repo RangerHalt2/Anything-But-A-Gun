b0a02b28568a0741f43e26164c206c2b987394ef
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2026-03-22 22:14:03 -0400

    Added AutoReload to Brush

--- Assets/Scripts/Weapons/BrushScript.cs
+++ Assets/Scripts/Weapons/BrushScript.cs
@@ -89,6 +89,21 @@ public class BrushScript : WeaponClass
                     }
                 }
             }
+            else if (ammoManager != null)
+            {
+                if (ammoManager.GetReserveAmmo() > 0 || ammoManager.GetReserveAmmo() == -1)
+                {
+                    ammoManager.ReloadWeapon();
+                }
+                else
+                {
+                    if (clickEffect != null && clickTimer <= 0)
+                    {
+                        clickTimer = clickCooldown;
+                        Instantiate(clickEffect, transform.position, transform.rotation, null);
+                    }
+                }
+            }
         }
     }
 
