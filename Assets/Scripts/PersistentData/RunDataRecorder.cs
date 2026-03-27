de5ff4a1d032ef2ab1fe14c69be10e3639f7a7bc
Hearthwright <ryan.lupoli@gmail.com>
2026-03-27 15:02:38 -0400

    Ending Grade can now pull from a persistent run data file

---
+++ Assets/Scripts/PersistentData/RunDataRecorder.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using System.IO;
+
+public class RunDataRecorder : MonoBehaviour
+{
+    public static RunDataRecorder Instance;
+
+    private RunData currentRunData = new RunData();
+
+    private string filePath;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        filePath = Path.Combine(Application.persistentDataPath, "runData.json");
+    }
+
+    #region Event Management
+    private void OnEnable()
+    {
+        GameEvent.RunStarted += StartNewRun;
+        GameEvent.OnEnemyKilled += OnEnemyKilled;
+        GameEvent.OnLevelCompleted += OnLevelCompleted;
+    }
+
+    private void OnDisable()
+    {
+        GameEvent.RunStarted -= StartNewRun;
+        GameEvent.OnEnemyKilled -= OnEnemyKilled;
+        GameEvent.OnLevelCompleted -= OnLevelCompleted;
+    }
+    #region Event Handlers
+    private void OnEnemyKilled()
+    {
+        currentRunData.enemiesKilled++;
+        SaveRunData();
+    }
+
+    private void OnLevelCompleted(string id)
+    {
+        currentRunData.levelsCompleted++;
+        SaveRunData();
+    }
+
+    #endregion
+    #endregion
+
+    #region Save / Load
+    // Save the current run data to the JSON
+    public void SaveRunData()
+    {
+        string json = JsonUtility.ToJson(currentRunData, true);
+        File.WriteAllText(filePath, json);
+    }
+
+    // Load data from the JSON to the current run
+    public void LoadRunData()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            currentRunData = JsonUtility.FromJson<RunData>(json);
+        }
+        else
+        {
+            currentRunData = new RunData();
+        }
+    }
+    #endregion
+
+    // Resets the RunData JSON
+    // Intended to be called at the start of every run
+    public void StartNewRun()
+    {
+        currentRunData = new RunData();
+        SaveRunData();
+    }
+
+    #region Getters
+    public int GetEnemiesKilled()
+    {
+        return currentRunData.enemiesKilled;
+    }
+
+    public int GetLevelsCompleted()
+    {
+        return currentRunData.levelsCompleted;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Weapons/BaseballBat_V2.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BaseballBat_V2 : WeaponClass
+{
+    [Header("Weapon Settings")]
+    //public int level { get; set; } = 1;
+    //[Tooltip("The base amount of damage the weapon deals before accounting for levels.")]
+    //[SerializeField] private float baseDamage = 15;
+    //[Tooltip("The amount of extra damage added for every level on the weapon.")]
+    //[SerializeField] private float levelDamage = 5f;
+    //public float cumulativeDamage;
+    //private WeaponLevel weaponLevel;
+
+    [Header("Swing Settings")]
+    [Tooltip("The amount of time (in Seconds) between swings.")]
+    [SerializeField] private float swingCooldown = 1f;
+    [Tooltip("The amount of time (in Seconds) the bat's hitbox will be active for.")]
+    [SerializeField] private float swingDuration = 0.3f;
+    private bool canSwing = true;
+    private bool firstHit = true;
+
+    //private AmmoManager ammoManager;
+
+    [Header("References")]
+    [Tooltip("Reference to the bat's hitbox. Should be a child of the weapon.")]
+    [SerializeField] private GameObject hitbox;
+    //[SerializeField] private GameObject hitEffect;
+    
+
+    void Start()
+    {
+        // If a hitbox is assigned, deactivate it
+        if (hitbox != null)
+        {
+            hitbox.SetActive(false);
+        }
+
+        // Get reference to the weapon level and ammo Manager
+        weaponLevel = GetComponent<WeaponLevel>();
+        ammoManager = GetComponent<AmmoManager>();
+    }
+
+    public override void Shoot()
+    {
+        // If player can swing...
+        if (canSwing)
+        {
+            CalculateLevelDamage();
+            // Start the swing coroutine
+            StartCoroutine(Swing());
+        }
+    }
+
+    private IEnumerator Swing()
+    {
+        canSwing = false;
+
+        // Enable hitbox
+        if (hitbox != null)
+        {
+            hitbox.SetActive(true);
+        }
+
+        // Wait for the duration of the swing
+        yield return new WaitForSeconds(swingDuration);
+
+        // Disable hitbox
+        if (hitbox != null)
+        {
+            hitbox.SetActive(false);
+        }
+
+        // Wait for the remaingin time the swing is on cooldown
+        yield return new WaitForSeconds(swingCooldown - swingDuration);
+
+        // Allow player to swing again
+        canSwing = true;
+        firstHit = true;
+    }
+
+    /*public void Reload()
+    {
+        if (ammoManager.GetReserveAmmo() > 0 || ammoManager.GetReserveAmmo() == -1) 
+        {
+            ammoManager.ReloadWeapon();
+        }
+    }*/
+
+    public void RegisterHit()
+    {
+        // Only allow this method to be called once per swing
+        if (firstHit)
+        {
+            firstHit = false;
+            // Decreas Ammo in ammo Manager
+            ammoManager.Fire();
+
+            // Play hit effect, if assigned
+            if (gunShot != null)
+            {
+                Instantiate(gunShot, transform.position, transform.rotation, null);
+            }
+        }
+    }
+
+    /*public void CalculateLevelDamage()
+    {
+        cumulativeDamage = baseDamage + levelDamage * (weaponLevel.Level - 1);
+        //Debug.Log("Weapon: Cumulative Damage " + cumulativeDamage);
+    }*/
+
+    public float GetCumulativeDamage()
+    {
+        return cumulativeDamage;
+    }
+}
