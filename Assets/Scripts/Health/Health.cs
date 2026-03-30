0376c41901e3663e04565f03621b365329fa9905
JaimeA47 <149345697+JaimeA47@users.noreply.github.com>
2026-03-30 04:04:58 -0400

    blood, laser and dash improvements

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -271,7 +271,7 @@ public class Health : MonoBehaviour
                 bloodVFX.transform.localScale = Vector3.one;
                 ParticleSystem.MainModule main = bloodVFX.main;
                 main.scalingMode = ParticleSystemScalingMode.Local;
-                Destroy(bloodVFX.gameObject, 6f); 
+                Destroy(bloodVFX.gameObject, 15f); 
             }
 
 
