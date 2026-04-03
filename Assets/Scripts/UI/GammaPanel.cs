b5d7458157070912b4e21c34c81f024c546b1221
Shawn <185832431+Shtealc@users.noreply.github.com>
2026-04-03 15:00:24 -0400

    gamma still working on it

---
+++ Assets/Scripts/UI/GammaPanel.cs
@@ -0,0 +1,35 @@
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+
+public class GammaPanel : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+     [SerializeField] GameObject panel;
+    public InputAction pauseAction;
+    [SerializeField] private InputActionAsset UIControls;
+
+    void Start()
+    {
+        var uiMap = UIControls.FindActionMap("UI", true);
+        pauseAction = uiMap.FindAction("Pause", true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Pause))
+        {
+            if (panel != null)
+            {
+                bool isActive = panel.activeSelf;
+                panel.SetActive(!isActive);
+            }
+
+        }
+    }
+}
