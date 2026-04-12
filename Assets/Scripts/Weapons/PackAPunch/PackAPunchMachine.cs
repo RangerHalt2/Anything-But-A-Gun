d98d4a89d9c97715bbffd8cd63a519b27a657240
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2026-04-11 22:13:02 -0400

    Economy changes again

--- Assets/Scripts/Weapons/PackAPunch/PackAPunchMachine.cs
+++ Assets/Scripts/Weapons/PackAPunch/PackAPunchMachine.cs
@@ -51,6 +51,7 @@ public class PackAPunchMachine : MonoBehaviour, IInteractable
             {
                 counter++;
                 GameEvent.OnWeaponModified?.Invoke();
+                EconomyManager.Instance.PTOAmount -= price;
             }
             if (counter == numOfUses)
             {
