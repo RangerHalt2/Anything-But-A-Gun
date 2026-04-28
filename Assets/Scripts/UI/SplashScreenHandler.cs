399181b60e33e233476e938d1f86e2363dd54c9d
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2026-04-28 02:07:29 -0400

    Fade Effect

--- Assets/Scripts/UI/SplashScreenHandler.cs
+++ Assets/Scripts/UI/SplashScreenHandler.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SplashScreenHandler : MonoBehaviour
 {
     [SerializeField] private float delay = 3f;
+    [SerializeField] private Image splashScreen;
+    private float timer = 0f;
 
     private void Start()
     {
-        if(delay < 0f) delay = 2f;
+        if (delay < 0f) delay = 2f;
         Invoke(nameof(DelayedStart), delay);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (splashScreen != null)
+        {
+            Color color = splashScreen.color;
+            if (timer <= (delay / 2))
+                color.a = Mathf.Lerp(0f, 1f, timer / (delay / 2));
+            if (timer > (delay / 2))
+                color.a = Mathf.Lerp(1f, 0f, timer / (delay / 2));
+            splashScreen.color = color;
+        }
     }
 
 
