3a81eb42bcb0c1c191402c2f44715ae8165d5ee8
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2026-04-28 22:26:29 -0400

    Fixed For Real

--- Assets/Scripts/UI/HitIndicator/HitIndicatorManager.cs
+++ Assets/Scripts/UI/HitIndicator/HitIndicatorManager.cs
@@ -12,20 +12,29 @@ public class HitIndicatorManager : MonoBehaviour
     private void Start()
     {
         playerController = GameObject.FindAnyObjectByType<PlayerController>();
-        playerHealth = playerController.GetComponent<Health>();
-        if (playerHealth != null)
+        if (playerController != null)
         {
-            Debug.Log("HIT INDICATOR MANAGER - Instance ID: " + playerHealth.GetInstanceID());
-            playerHealth.PlayerTookDamage += SpawnIndicator;
-            Debug.Log("HIT INDICATOR MANAGER - Player Controller, Health, and Listeners are complete");
-            Debug.Log("HIT INDICATOR MANAGER - Subscribed: " + playerHealth.HasDamageListeners());
+            playerHealth = playerController.GetComponent<Health>();
+            if (playerHealth != null)
+            {
+                Debug.Log("HIT INDICATOR MANAGER - Instance ID: " + playerHealth.GetInstanceID());
+                playerHealth.PlayerTookDamage += SpawnIndicator;
+                Debug.Log("HIT INDICATOR MANAGER - Player Controller, Health, and Listeners are complete");
+                Debug.Log("HIT INDICATOR MANAGER - Subscribed: " + playerHealth.HasDamageListeners());
+            }
         }
+
     }
 
     //public void Confirm
 
     private void Update()
     {
+        if (playerController == null)
+        {
+            playerController = GameObject.FindAnyObjectByType<PlayerController>();
+        }
+
         if (playerHealth == null)
         {
             playerHealth = playerController.GetComponent<Health>();
@@ -40,6 +49,20 @@ public class HitIndicatorManager : MonoBehaviour
 
     private void SpawnIndicator(Health.DamageInfo info)
     {
+        if (spawnPoint == null)
+        {
+            HitIndicatorSpawnPoint spawnIndicatorPoint = GameObject.FindAnyObjectByType<HitIndicatorSpawnPoint>();
+            if (spawnIndicatorPoint != null)
+            {
+                spawnPoint = spawnIndicatorPoint.gameObject.GetComponent<RectTransform>();
+            }
+        }
+
+        if (spawnPoint == null)
+            {
+                Debug.LogError("HIT INDICATOR MANAGER - SPAWN POINT IS STILL NULL");
+                return;
+            }
         GameObject spawnedHit = Instantiate(hitIndicatorPrefab, spawnPoint.transform);
         spawnedHit.transform.localScale = Vector3.one;
         spawnedHit.transform.localPosition = Vector3.zero;
